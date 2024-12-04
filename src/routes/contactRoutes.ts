055eddefef038403a6624629d3aca5201c544c9e
Ewerton Reis <35968653-ewertonreis991@users.noreply.replit.com>
2024-12-04 23:52:50 +0000

    feat: adicionei routes e server

--- src/routes/contactRoutes.ts
+++ src/routes/contactRoutes.ts
@@ -4,11 +4,8 @@ import { getContacts, postContact, putContact, deleteContact } from '../controll
 const router = Router();
 
 router.get('/contatos', getContacts);
-
 router.post('/contatos', postContact);
-
 router.put('/contacts_atu/:id', putContact);
-
 router.delete('/contacts_del/:id', deleteContact);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
-//import contactRoutes from './routes/contactRoutes';
+import contactRoutes from './routes/contactRoutes';
 
 const app = express();
 
 app.use(express.json());
-
-//app.use('/api', contactRoutes);
+app.use(contactRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
