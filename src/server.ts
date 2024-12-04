ccf159fcdfa51c2842bbfa0c81c1de538fc80a9b
Ewerton Reis <35968653-ewertonreis991@users.noreply.replit.com>
2024-12-04 23:57:58 +0000

    fix: arrumei erro de digitação

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import contactRoutes from './routes/contactRoutes';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(contactRoutes);
 
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
